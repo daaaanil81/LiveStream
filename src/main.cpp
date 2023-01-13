e70d784b95726cd1db641699bd1800de05cd36f6
Daniil Petrov <d.petrov@mobidev.biz>
2023-01-13 16:47:15 +0200

    Add necessary headers
    Signed-off-by: Daniil Petrov <d.petrov@mobidev.biz>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+#ifdef __cplusplus
+}
+#endif
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+
 int main(int argc, char *argv[]) {
 
     return 0;
